bc8e02c9726e792e6952c939b3bd90d9aba4e7d2
Madhurima Nath <31106009+madhurima-nath@users.noreply.github.com>
2024-02-12 21:34:39 -0500

    added download file script

---
+++ awsRelated/extract/download.py
@@ -0,0 +1,12 @@
+import boto3
+
+def get_aws_client(profile_name: str) -> boto3.Session:
+    session = boto3.Session(profile_name=profile_name)
+    return session
+
+def download_file(
+        session: boto3.Session, bucket: str, object_name: str, output_path: str
+) -> None:
+    with open(output_path, "wb") as binary_file:
+        s3_client = session.client("s3")
+        s3_client.download_fileobj(bucket, object_name, binary_file)
